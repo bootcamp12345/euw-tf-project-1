9cb1561ffd1e59ea855af8909297d7dfcfcbd14a
cody-carlson <78826051+cody-carlson@users.noreply.github.com>
2023-03-16 09:09:45 -0600

    Pr 4 (#4)
    * adding outputs from modules to state file
    
    * updating pipeline triggers, fixing syntax on main outputs
    
    * ado pipeline triggers
    
    * trigger syntax on azure-pipeline.yaml
    
    * updating pipeline files to start using a template
    
    * moving pipeline files
    
    * Update tf-init-plan.yaml for Azure Pipelines
    
    * Update tf-init-plan.yaml for Azure Pipelines
    
    * template syntax
    
    * syntax
    
    * updating pipelines with new InstallSSHKey Task in Templates
    
    * updating parameters to have types in template
    
    * trying different syntax for tf-app-subscription-id in template
    
    * trying different syntax for tf-app-subscription-id in template 2
    
    * new syntax that shold work
    
    * new syntax that should work 2
    
    * updating parameter references
    
    * updating tf job name syntax in template
    
    * Adding new parameter for AZ Login
    
    * error on line 57 referencing incorrect parameter
    
    * Fixing typo to incorrect parameter on line 70
    
    * updating parameter files
    
    * Updating Template & Parameters for TF-Apply

--- config/uat-poc/parameters.tf
+++ config/uat-poc/parameters.tf
@@ -4,4 +4,14 @@ resource_group_name   = "asp-rg"
 app_service_plan_name = "asptoday1234"
 asp_os_type           = "Linux"
 asp_sku               = "F1"
-
+resource_groups = {
+  resource_group_1 = {
+    name     = "asp-uat-rg"
+    location = "West Europe"
+    tags = {
+      created_by = "codycarlson"
+      contact_dl = "codycarlson@microsoft.com"
+      app_id    = "00000"
+    }
+  }
+}
